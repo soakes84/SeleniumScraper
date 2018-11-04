8c73f9b534f2c5aee697c58d9afeaa02b81515d6
soakes84 <spencer.oakes84@gmail.com>
2018-11-04 11:22:19 -0500

    Add project files.

---
+++ SeleniumScraper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeleniumScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
